91b4844740142cb86206e0bf7cb41f6c3f58a2cd
Meowyam <maryam@chilon.net>
2020-11-11 14:16:39 +0800

    add cnf

--- bool.hs
+++ bool.hs
@@ -30,3 +30,14 @@ nnf (Not (f1 `Or` f2)) = nnf (Not f1) `And` nnf (Not f2)
 nnf (Not (f1 `And` f2)) = nnf (Not f1) `Or` nnf (Not f2)
 nnf (Not (Not f)) = f
 nnf (Not f) = Not f
+
+distribOr :: Form -> Form -> Form
+distribOr f1 f2 = f1 `Or` f2
+
+cnf :: Form -> Form
+cnf ((f1 `And` f2) `Or` (g1 `And` g2))
+  = (distribOr (cnf f1) (cnf g1)) `And` (distribOr (cnf f2) (cnf g2))
+    `And` ((distribOr (cnf f1) (cnf g2)) `And` (distribOr (cnf f2) (cnf g1)))
+cnf (f `And` (g1 `Or` g2))
+  = (distribOr (cnf f) (cnf g1)) `And` (distribOr (cnf f) (cnf g2))
+cnf f = f
